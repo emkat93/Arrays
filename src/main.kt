246cf032c8cf0b4b06daa5f2f66a115016614eae
emkat93 <eallen5064@students.mchenry.edu>
2020-09-05 00:02:54 -0500

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var HotdogStand = arrayOf("hot dog", "pretzel", "fries", "corn dog", "ketchup", "mustard", "cans of soda",
+        "bottles of juice", "napkins", "paper food trays")
+
+    for (singlePart in HotdogStand){
+        println("${singlePart}")
+    }
+}
